e41aa80a560724040f549f86c131edd15d5218c9
Hitesh <hitesh.yadav@vaaaninfra.com>
2023-08-21 11:44:54 +0530

    Add project files.

---
+++ SimpleJWT/Controllers/AuthController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace SimpleJWT.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        IConfiguration configuration;
+        public AuthController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public IActionResult Auth([FromBody] User user)
+        {
+            IActionResult response = Unauthorized();
+            if(user != null)
+            {
+                if(user.UserName.Equals("test@email.com") && user.Password.Equals("a"))
+                {
+                    var issuer = configuration["Jwt:Issuer"];
+                    var audience = configuration["Jwt:Audience"];
+                    var key = Encoding.UTF8.GetBytes(configuration["Jwt:key"]);
+                    var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+
+                    var subject = new ClaimsIdentity(new[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Sub,user.UserName),
+                        new Claim(JwtRegisteredClaimNames.Email,user.UserName)
+                        
+                    }); 
+
+                    var expires = DateTime.UtcNow.AddMinutes(5);
+
+                    var tokenDescriptor = new SecurityTokenDescriptor
+                    {
+                        Subject = subject,
+                        Issuer = issuer,
+                        Expires = expires,
+                        Audience = audience,
+                        SigningCredentials = signingCredentials
+                    };
+
+                    var tokenHandler = new JwtSecurityTokenHandler();
+                    var token = tokenHandler.CreateToken(tokenDescriptor);
+                    var jwtToken = tokenHandler.WriteToken(token);
+                    
+
+                    return Ok(jwtToken);
+                }
+            }
+            return response;
+        }
+    }
+}
